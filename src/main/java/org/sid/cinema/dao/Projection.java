974723c053371dca4460624fec4b9c52087034cb
Oumaiyma INTISSAR <49239111+OumaIntissar@users.noreply.github.com>
2021-09-26 22:34:45 +0100

    Add files via upload

---
+++ src/main/java/org/sid/cinema/dao/Projection.java
@@ -0,0 +1,34 @@
+package org.sid.cinema.dao;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Projection implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private Date dateProjection;
+	private double prix;
+	@ManyToOne
+	private Film film;
+	@ManyToOne
+	private Salle salle;
+	@ManyToOne
+	private Seance seance;
+
+}
