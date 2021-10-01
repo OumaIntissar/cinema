c6ea8dfd8b96d066742a52f254b217dcc3c9a861
OumaIntissar <oumaintissar@gmail.com>
2021-10-01 22:43:02 +0100

    cinemas list

---
+++ cinema-frontend/src/app/components/cinemas-list/cinemas-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cinemas-list',
+  templateUrl: './cinemas-list.component.html',
+  styleUrls: ['./cinemas-list.component.css']
+})
+export class CinemasListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
